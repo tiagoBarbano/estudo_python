c7aa2d566e0c9909fc239dde2a259a645acc6b5a
tiagoBarbano <tiago.barbano@gmail.com>
2022-11-09 13:33:51 -0300

    ajuste Crud SQL

--- 04-Integracao_DB_SQL/app/repository.py
+++ 04-Integracao_DB_SQL/app/repository.py
@@ -1,31 +1,19 @@
-from app.schema import UserSchema
+from app.schema import UserSchema, UserSchemaUpdate
 from app.model import UserModel
 from sqlalchemy import update, delete
 from sqlalchemy.future import select
 from sqlalchemy.ext.asyncio import AsyncSession
-from sqlalchemy.exc import SQLAlchemyError
-from fastapi import HTTPException, status
-from app.database import async_session
 
-# Retrieve all users present in the database
-async def get_all_users():
-    try:
-        async with async_session() as session:
-            query = select(UserModel)
-            users = await session.execute(query)
-            users = users.scalars().all()
-            await session.close()
-            return users
-    except SQLAlchemyError as sql_ex:
-        await session.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(sql_ex))
-    except HTTPException as http_ex:
-        await session.rollback()
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(http_ex))
-    finally:
-        await session.close()    
-    
-# Retrieve a user with a matching ID
+
+async def get_all_users(db: AsyncSession):
+    async with db as session:
+        query = select(UserModel)
+        users = await session.execute(query)
+        users = users.scalars().all()
+        await session.close()
+        return users
+
+
 async def get_user_by_id(db: AsyncSession, id: int) -> dict:
     query = select(UserModel).where(UserModel.id == id)
     users = await db.execute(query)
@@ -33,33 +21,35 @@ async def get_user_by_id(db: AsyncSession, id: int) -> dict:
     await db.close()
     return user
 
-# Add a new user into to the database
-async def add_user(db: AsyncSession, user_data: UserSchema) -> UserSchema:
-    new_user = UserModel(nome=user_data.nome, idade=user_data.idade, email=user_data.email)
-    await db.add(new_user)
+
+async def add_user(db: AsyncSession,
+                   user_data: UserSchemaUpdate) -> UserSchema:
+    new_user = UserModel(nome=user_data.nome,
+                         idade=user_data.idade,
+                         email=user_data.email)
+    db.add(new_user)
     await db.commit()
-    await db.close()
     return new_user
 
-# Update a user with a matching ID
-async def update_user(db: AsyncSession, id: str, data: UserSchema):
-    query = (update(UserModel).where(UserModel.id == id).values(data).execution_options(synchronize_session="fetch"))
-    
-    await db.execute(query)
-    
-    try:
-        await db.commit()
-    except Exception:
-        await db.rollback()
-        raise
 
-# Delete a user from the database
+async def update_user(db: AsyncSession, id: str, data: UserSchemaUpdate):
+    query = (update(UserModel).where(UserModel.id == id).values(
+        data.dict()).execution_options(synchronize_session="fetch"))
+    updated_user = await db.execute(query)
+
+    if updated_user:
+        await db.commit()
+        return True
+
+    return False
+
+
 async def delete_user(db: AsyncSession, id: int):
     query = delete(UserModel).where(UserModel.id == id)
-    await db.execute(query)
-    try:
+    res = await db.execute(query)
+
+    if res.rowcount == 1:
         await db.commit()
-    except Exception:
-        await db.rollback()
-        raise
-    return True
+        return True
+
+    return False
--- 04-Integracao_DB_SQL/app/service.py
+++ 04-Integracao_DB_SQL/app/service.py
@@ -2,33 +2,20 @@ from fastapi import APIRouter, Body, HTTPException, status, Depends
 from app.schema import UserSchema, UserSchemaUpdate
 from app.database import get_db
 from sqlalchemy.ext.asyncio import AsyncSession
-from app.model import UserModel
-from sqlalchemy import update, delete
-from sqlalchemy.future import select
-# from fastapi_pagination.ext.sqlalchemy_future import paginate
-# from fastapi_pagination import Page, paginate, LimitOffsetPage
+from app.repository import *
+
 
 router = APIRouter()
 
 
-#@router.get("/", status_code=status.HTTP_200_OK, response_model=Page[UserSchema])
-#@router.get("/limit-offset", response_model=LimitOffsetPage[UserSchema])
 @router.get("/", status_code=status.HTTP_200_OK, response_model=list[UserSchema])
 async def get_users_default(db: AsyncSession = Depends(get_db)):
-    #return await paginate(db, select(UserModel))
-    async with db as session:
-        query = select(UserModel)
-        users = await session.execute(query)
-        users = users.scalars().all()
-
-        return users
+    return await get_all_users(db)
 
 
 @router.get("/{id}")
 async def get_user_data(id: int, db: AsyncSession = Depends(get_db)):
-    query = select(UserModel).where(UserModel.id == id)
-    users = await db.execute(query)
-    user = users.scalar_one_or_none()
+    user = await get_user_by_id(db, id)
 
     if user:
         return user
@@ -39,44 +26,43 @@ async def get_user_data(id: int, db: AsyncSession = Depends(get_db)):
 @router.post("/",
              status_code=status.HTTP_201_CREATED,
              response_model=UserSchema)
-async def add_user(user: UserSchemaUpdate = Body(...),
+async def new_user(user: UserSchemaUpdate = Body(...),
                    db: AsyncSession = Depends(get_db)):
-    new_user = UserModel(nome=user.nome, idade=user.idade, email=user.email)
-    db.add(new_user)
-    await db.commit()
-    return new_user
+    try:
+        return await add_user(db, user)
+    except HTTPException:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+                            detail="User ErroR")    
 
 
 @router.put("/{id}", status_code=status.HTTP_200_OK)
 async def update_user_data(id: int,
                            req: UserSchemaUpdate = Body(...),
                            db: AsyncSession = Depends(get_db)):
-    query = (update(UserModel).where(
-        UserModel.id == id).values(req.dict()).execution_options(
-            synchronize_session="fetch"))
-    updated_user = await db.execute(query)
+    try:
+        flag_update = await update_user(db, id, req)
 
-    if updated_user:
-        await db.commit()
-        return HTTPException(status_code=status.HTTP_200_OK,
-                        detail="User Updated")
+        if flag_update :
+            return {"message" : "User Updated"}
 
-    raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                        detail="User not found")
-
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail="User not found")
+    except HTTPException as ex:
+        raise HTTPException(status_code=ex.status_code,
+                            detail=ex.detail)
 
 @router.delete("/{id}",
                status_code=status.HTTP_200_OK)
 async def delete_user_data(id: int, db: AsyncSession = Depends(get_db)):
-    query = delete(UserModel).where(UserModel.id == id)
-    await db.execute(query)
-
     try:
-        await db.commit()
-        return HTTPException(status_code=status.HTTP_200_OK,
-                        detail="User Deleted")
-    except Exception:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
-                            detail="User ErroR")
+        flag_delete = await delete_user(db, id)
+        if flag_delete:
+            return {"message" : "User Deleted"}
+        
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                        detail="User not found")
+    except HTTPException as ex:
+        raise HTTPException(status_code=ex.status_code,
+                            detail=ex.detail)
 
 
