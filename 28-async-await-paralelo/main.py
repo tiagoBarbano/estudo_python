d7e9631ab1fd1ee91ac15c88bc86edef8a28edcc
tiagoBarbano <tiago.barbano@gmail.com>
2023-01-16 22:55:21 -0300

    add

---
+++ 28-async-await-paralelo/main.py
@@ -0,0 +1,42 @@
+from fastapi import FastAPI
+import asyncio
+
+
+async def calculo1():
+    print("Inicio Calculo1")
+    await asyncio.sleep(1)
+    print("Termino Calculo1")
+    return 1
+
+async def calculo2():
+    print("Inicio Calculo2")
+    await asyncio.sleep(1)
+    print("Termino Calculo2")
+    return 2
+
+async def calculo3():
+    print("Inicio Calculo3")
+    await asyncio.sleep(1)
+    print("Termino Calculo3")
+    return 3
+
+app = FastAPI()
+
+#Busca um item através do ID
+@app.get("/calc_async")
+async def read_item():
+    c1 = await calculo1()
+    c2 = await calculo2()
+    c3 = await calculo3()
+    
+    return c1 + c2 + c3
+
+
+#Busca um item através do ID
+@app.get("/calc_asyncio")
+async def read_item():
+    c1, c2, c3 = await asyncio.gather(calculo1(), 
+                                      calculo2(), 
+                                      calculo3())
+
+    return c1 + c2 + c3
---
+++ 28-async-await-paralelo/readme.md
@@ -0,0 +1,1 @@
+Exemplo de chamadas Assincronas e em paralelo.
